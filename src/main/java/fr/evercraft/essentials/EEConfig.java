c5ae3438780989f9bbd3672210d9f9b473777523
lesbleu <lesbleu@evercraft.fr>
2016-07-15 05:07:20 +0200

    Update EverEssentials

--- src/main/java/fr/evercraft/essentials/EEConfig.java
+++ src/main/java/fr/evercraft/essentials/EEConfig.java
@@ -34,7 +34,7 @@ public class EEConfig extends EConfig {
 	@Override
 	public void loadDefault() {
 		addDefault("debug", false, "Displays plugin performance in the logs");
-		addDefault("language", EMessage.ENGLISH, "Select language messages", "Examples : ", "  French : FR_fr", "  English : EN_en");
+		addDefault("language", EMessage.FRENCH, "Select language messages", "Examples : ", "  French : FR_fr", "  English : EN_en");
 		
 		// SQL
 		addComment("SQL", 	"Save the user in a database : ",
