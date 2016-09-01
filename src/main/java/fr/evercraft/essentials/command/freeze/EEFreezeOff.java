62e2b1aae1dcc4b4271e6c870e8e93c11e5f4909
rexbut <butbutdu45@gmail.com>
2016-09-01 09:01:39 +0200

    Signed-off-by: rexbut <butbutdu45@gmail.com>

--- src/main/java/fr/evercraft/essentials/command/freeze/EEFreezeOff.java
+++ src/main/java/fr/evercraft/essentials/command/freeze/EEFreezeOff.java
@@ -69,7 +69,7 @@ public class EEFreezeOff extends ESubCommand<EverEssentials> {
 	@Override
 	public List<String> subTabCompleter(final CommandSource source, final List<String> args) throws CommandException {
 		List<String> suggests = new ArrayList<String>();
-		if (args.size() == 1 && source.hasPermission(EEPermissions.FREEZE_OTHERS.get())){
+		if (!(args.size() == 0 && source.hasPermission(EEPermissions.FREEZE_OTHERS.get()))){
 			suggests.addAll(this.getAllUsers());
 		}
 		return suggests;
